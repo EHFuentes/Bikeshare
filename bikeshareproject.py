34f9b8b52d7cc05e51e36c2f0fda7b1b8fa0e42b
edwinhfuentes <helloedwinf@gmail.com>
2022-02-14 12:56:22 -0500

    Style: fixed missed doctring in get filters

--- bikeshareproject.py
+++ bikeshareproject.py
@@ -19,15 +19,6 @@ city_data = { 'chicago': chicago,
               'new york city': newyorkcity,
               'washington': washington}
 
-
-"""
-    Asks user to specify a city, month, and day to analyze. 
-    Returns:
-        (str) city - name of the city to analyze
-        (str) month - name of the month to filter by, or "all" to apply no month filter
-        (str) day - name of the day of week to filter by, or "all" to apply no day filter
-"""
-
 # Get user input to select a city (chicago, new york city, washington)
 def get_filters():
     """
